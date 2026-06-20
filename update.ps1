c96a00b7af81950b10652d16ccd5197334994064
shigeonagase1965 <shigeonagase1965@gmail.com>
2026-06-20 23:29:07 +0900

    繝帙・繝繝壹・繧ｸ譖ｴ譁ｰ

---
+++ update.ps1
@@ -0,0 +1,17 @@
+cd D:\ai-public2
+
+git status
+
+git add .
+
+git commit -m "ホームページ更新"
+
+git push
+
+Write-Host ""
+Write-Host "==============================="
+Write-Host "GitHubへの更新が完了しました。"
+Write-Host "Renderの反映を確認してください。"
+Write-Host "==============================="
+
+pause
